e18e73e6cf77d7167cbbecf3e833574e80974988
JoanneOs <jowan.awsman@yahoo.com>
2025-02-26 21:09:51 -0600

    if

--- js.js
+++ js.js
@@ -8,7 +8,19 @@ console.log("radius= " + radius);
 console.log("pi= " + pi);
 console.log("area= " + area);
 //Each plant requires a minimum space of 0.8 square meters.
+let plantSpace=0.8;
+console.log("plantSpace = " + plantSpace);
 
 //The area is starting with 20 plants.
-
+let areaStart=20;
+console.log("areaStart = " + areaStart);
 //The plants double in number every week.
+let plantGrowth= areaStart;
+console.log("plantGrowth = " + plantGrowth);
+//Predict the plant growth after a specific number of weeks.
+let week=3;
+
+if (week>0){plantGrowth=plantGrowth*2; week--;
+}
+console.log("plantGrowth after " + week + " weeks = "+ plantGrowth);
+
