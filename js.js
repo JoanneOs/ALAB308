0e66da781921b365b7ec39f7a3c6e5dbce478158
JoanneOs <jowan.awsman@yahoo.com>
2025-02-26 20:51:31 -0600

    givings

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
 console.log("hi");
+
+let radius = 5;
+const pi = 3.1415;
+const area = pi * radius;
+//output givings
+console.log("radius= " + radius);
+console.log("pi= " + pi);
+console.log("area= " + area);
+//Each plant requires a minimum space of 0.8 square meters.
+
+//The area is starting with 20 plants.
+
+//The plants double in number every week.
